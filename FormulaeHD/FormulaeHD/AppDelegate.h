1b5b7d914ee1d33e9837a4233318020ea24b8fbd
sammovale <sammovale@hotmail.com>
2013-06-02 13:22:51 +0930

    Initial Commit
    Added all files for Formulae HD

---
+++ FormulaeHD/FormulaeHD/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  FormulaeHD
+//
+//  Created by David McAfee on 3/05/13.
+//  Copyright (c) 2013 David McAfee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ FormulaeHD/FormulaeHD/ViewController.h
@@ -0,0 +1,43 @@
+//
+//  ViewController.h
+//  FormulaeHD
+//
+//  Created by David McAfee on 3/05/13.
+//  Copyright (c) 2013 David McAfee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <MediaPlayer/MediaPlayer.h>
+
+@interface ViewController : UIViewController {
+    
+IBOutlet UIWebView *Webview;
+    IBOutlet UIImageView *formulae;
+    
+}
+
+-(IBAction)slopederiv;
+-(IBAction)factor:(id)sender;
+-(IBAction)trig:(id)sender;
+-(IBAction)compoundint:(id)sender;
+-(IBAction)astrophysics;
+-(IBAction)energyform;
+-(IBAction)geometry;
+-(IBAction)chemical;
+-(IBAction)quadsaved;
+-(IBAction)fractions;
+-(IBAction)triger;
+-(IBAction)logexpo;
+-(IBAction)logger:(id)sender;
+-(IBAction)fce;
+
+-(IBAction)line:(id)sender;
+
+-(IBAction)graphInter:(id)sender;
+-(IBAction)quadQuiz:(id)sender;
+@property(nonatomic,retain)IBOutlet UIImageView *formulae;
+
+-(IBAction)quadCon:(id)sender;
+
+-(IBAction)stats:(id)sender;
+@end
