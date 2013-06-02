609f91e6e9d3504fc09ca4f3380072d735f1030a
sammovale <sammovale@hotmail.com>
2013-06-02 13:41:44 +0930

    Intial Commit that works!

---
+++ FormulaeHD/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  FormulaeHD
+//
+//  Created by David McAfee on 3/05/13.
+//  Copyright (c) 2013 David McAfee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
